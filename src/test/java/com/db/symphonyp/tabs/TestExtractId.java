8b3fb626f48dfade10bf43d17a097db5217299d3
Rob Moffat <robmoffat@mac.com>
2019-09-12 10:44:30 +0100

    Example table

--- src/test/java/com/db/symphonyp/tabs/TestExtractId.java
+++ src/test/java/com/db/symphonyp/tabs/TestExtractId.java
@@ -6,8 +6,6 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -16,7 +14,7 @@ import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment=WebEnvironment.RANDOM_PORT)
+//@SpringBootTest(webEnvironment=WebEnvironment.RANDOM_PORT)
 @TestPropertySource(properties = { 
 		"settings.file=file:target/test-settings.json"})
 public class TestExtractId {
